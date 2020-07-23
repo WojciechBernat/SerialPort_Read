58eb6e66d792dad41193274766e55749aace3e1e
Deuter <wojciechbernat95@gmail.com>
2020-07-23 20:29:36 +0200

    Added buffer and their print methodes

--- SerialPort_Write_Objects.py
+++ SerialPort_Write_Objects.py
@@ -9,17 +9,28 @@ class serialPort:
     # port: None
     # baudrate = 115200
     # the rest by default
+
     def __init__(self, _serialClass = serial.Serial(None, 115200) ):
         self._serialClass = _serialClass  #serial port class from PySerial
+        self._writeBuffer = []
+        self._readingBuffer = []
 
     # print logs function
     def initLog(self):
         print("Passed serial port object: " + str(self._serialClass))
 
+    def printReadingBuffer(self):
+        print("Reading Buffer content: " + str(self._readingBuffer))
+
+    def printWritingBuffer(self):
+        print("Writing Buffer content: " + str(self._writeBuffer))
 
 arduinoSerialPort = serial.Serial('COM3', 115200)
-print("Serial port class" + str(arduinoSerialPort))
 test_object = serialPort(arduinoSerialPort)
 
-print("General serial port object: " + str(test_object))
+# print("Serial port class" + str(arduinoSerialPort))
+# print("General serial port object: " + str(test_object))
+
 test_object.initLog()
+test_object.printReadingBuffer()
+test_object.printWritingBuffer()
