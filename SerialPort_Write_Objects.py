bcd958965e96e822a11abe0b7116eba889867988
Deuter <wojciechbernat95@gmail.com>
2020-07-25 16:10:39 +0200

    Added main buffer as container for user arrays

--- SerialPort_Write_Objects.py
+++ SerialPort_Write_Objects.py
@@ -1,105 +1,11 @@
 import time
 import serial
 #import serial.tools.list_ports
-
-#special class for serial port
-class SpecSerialPort:
-    #constructor - init pyserial class
-    # default:
-    # port: None
-    # baudrate = 115200
-    # the rest by default
-
-    __commandList ={0xAAA: "BeginCmd",
-                    0xAAB: "EndCmd",
-                    0xAAC: "TelemetryCmd"}
-
-    __rwTimeOut = 1
-
-    def __init__(self, _serialClass = serial.Serial(None, 115200) ):
-        self._serialClass = _serialClass  #serial port class from PySerial
-        self.__writeBuffer = []
-        self.__readingBuffer = []
-
-    # print logs function
-    def initLog(self):
-        print("Passed serial port object: " + str(self._serialClass))
-
-    @property
-    def ReadingBuffer(self):
-        return self.__readingBuffer
-        print("Reading Buffer content: " + str(self.__readingBuffer))
-
-    @ReadingBuffer.getter
-    def readingBuffer(self):
-        print("Reading Buffer content: " + str(self.__readingBuffer))
-
-    @readingBuffer.setter
-    def readingBuffer(self, data):
-        self.__readingBuffer.append(data)
-
-    @property
-    def WritingBuffer(self):
-        return self.__writeBuffer
-
-    @WritingBuffer.getter
-    def writingBuffer(self):
-        print("Writing Buffer content: " + str(self.__writeBuffer))
-
-    @writingBuffer.setter
-    def writingBuffer(self, data):
-        self.__writeBuffer.append(data)
-
-    @property
-    def getCommandDictionary(self):
-        for i, j in self.__commandList.items():
-            print("Hex code: {0}, Name: {1}".format(hex(i), j))
-
-    def addCommand(self, newCmdName):
-        if type(newCmdName) != str:
-            newCmdName = str(newCmdName)
-        __newCmdKey = list(self.__commandList.keys())[-1]
-        __newCmdKey += 1
-        self.__commandList[__newCmdKey] = newCmdName
-
-    def removeCommand(self, cmdName):
-        if type(cmdName) != str:
-            cmdName = str(cmdName)
-
-        notFoundCounter = 0;
-        dictLen = len(self.__commandList)
-        for key,val in self.__commandList.items():
-            if val == cmdName:
-                print("Found command to remove. \nKey: {0}, Name: {1}".format( str(key), str(val)))
-                self.__commandList.pop(key)
-                break #break loop executing - found command to remove
-
-            notFoundCounter += 1
-            if notFoundCounter == (dictLen):
-                print("Command to remove not found.")
-                break  # break loop - command not found
-
-    @property
-    def readWriteTimeOut(self):
-        return self.__rwTimeOut
-
-    @readWriteTimeOut.getter
-    def readWriteTimeOut(self):
-        print("Read/Write time out: " + str(self.__rwTimeOut))
-
-    @readWriteTimeOut.setter
-    def readWriteTimeOut(self, timeout):
-        try:
-            if (timeout < 0):
-                raise ValueError
-            else:
-                self.__rwTimeOut = timeout
-        except ValueError:
-            print("Invalid time out value. \nValue less than 0!")
+from SpecialSerialPort import SpecialSerialPort
 
 
 arduinoSerialPort = serial.Serial('COM3', 115200)
-test_object = SpecSerialPort(arduinoSerialPort)
+test_object = SpecialSerialPort(arduinoSerialPort)
 
 # test_object.initLog()
 # test_object.getReadingBuffer
@@ -115,11 +21,26 @@ test_object = SpecSerialPort(arduinoSerialPort)
 # test_object.ReadWriteTimeOut
 # test_object.ReadWriteTimeOut = -100
 # test_object.ReadWriteTimeOut
+#
+# test_object.readingBuffer
+# test_object.readingBuffer = 10
+# test_object.readingBuffer
+#
+# test_object.writingBuffer
+# test_object.writingBuffer = 100
+# test_object.writingBuffer
 
-test_object.readingBuffer
-test_object.readingBuffer = 10
-test_object.readingBuffer
+# test_object.commandDictionary
+# test_object.commandDictionary = "GetTemp"
+# test_object.commandDictionary
+# test_object.removeCommand("GetTemp")
+# test_object.commandDictionary
 
-test_object.writingBuffer
-test_object.writingBuffer = 100
-test_object.writingBuffer
+test_object.mainDataBuffer
+test = [0,1,2,3,4,5]
+arr = ["ArgL", "ArgC"]
+test_object.mainDataBuffer = test
+test_object.mainDataBuffer = arr
+test_object.mainDataBuffer
+del test_object.mainDataBuffer
+test_object.mainDataBuffer
---
+++ SpecialSerialPort.py
@@ -0,0 +1,135 @@
+import time
+#import serial.tools.list_ports
+import serial
+
+class SpecialSerialPort:
+    #constructor - init pyserial class
+    # default:
+    # port: None
+    # baudrate = 115200
+    # the rest by default
+
+    __commandList ={0xAAA: "BeginCmd",
+                    0xAAB: "EndCmd",
+                    0xAAC: "TelemetryCmd"}
+    __rwTimeOut = 1
+
+    def __init__(self, _serialClass = serial.Serial(None, 115200) ):
+        self._serialClass = _serialClass  #serial port class from PySerial
+        self.__writeBuffer = []
+        self.__readingBuffer = []
+        self.__mainDataBuffer = []
+
+    #main data buffer in place where date will be copy after receive
+    #in this array you can add your specify buffer.
+    # i.e. __mainDataBuffer = [temperature, voltage, speed, preasure]
+
+    # print logs function
+    def initLog(self):
+        print("Passed serial port object: " + str(self._serialClass))
+
+    #reading buffer
+    @property
+    def readingBuffer(self):
+        return self.__readingBuffer
+        print("Reading Buffer content: " + str(self.__readingBuffer))
+
+    @readingBuffer.getter
+    def readingBuffer(self):
+        print("Reading Buffer content: " + str(self.__readingBuffer))
+
+    @readingBuffer.setter
+    def readingBuffer(self, data):
+        self.__readingBuffer.append(data)
+
+    @readingBuffer.setter
+    def readingBuffer(self):
+        self.__readingBuffer.clear()
+
+    #writing buffer
+    @property
+    def writtingBuffer(self):
+        return self.__writeBuffer
+
+    @writtingBuffer.getter
+    def writtingBuffer(self):
+        print("Writting Buffer content: " + str(self.__writeBuffer))
+
+    @writtingBuffer.setter
+    def writtingBuffer(self, data):
+        self.__writeBuffer.append(data)
+
+    @writtingBuffer.deleter
+    def writtingBuffer(self):
+        self.__writeBuffer.clear()
+
+    ##main data buffer
+    @property
+    def mainDataBuffer(self):
+        return self.__mainDataBuffer
+
+    @mainDataBuffer.getter
+    def mainDataBuffer(self):
+        print("Main data buffer content: " + str(self.__mainDataBuffer))
+
+    @mainDataBuffer.setter
+    def mainDataBuffer(self, arg):
+        return self.__mainDataBuffer.append(arg)
+
+    @mainDataBuffer.deleter
+    def mainDataBuffer(self):
+        return self.__mainDataBuffer.clear()
+
+    #command's dictionary
+    @property
+    def commandDictionary(self):
+        print(self.__commandList)
+
+    @commandDictionary.getter
+    def commandDictionary(self):
+        for i, j in self.__commandList.items():
+            print("Hex code: {0}, Name: {1}".format(hex(i), j))
+
+    @commandDictionary.setter
+    def commandDictionary(self, newCmdName):
+        if type(newCmdName) != str:
+            newCmdName = str(newCmdName)
+        __newCmdKey = list(self.__commandList.keys())[-1]
+        __newCmdKey += 1
+        self.__commandList[__newCmdKey] = newCmdName
+
+    def removeCommand(self, cmdName):
+        if type(cmdName) != str:
+            cmdName = str(cmdName)
+
+        notFoundCounter = 0;
+        dictLen = len(self.__commandList)
+        for key,val in self.__commandList.items():
+            if val == cmdName:
+                print("Found command to remove. \nKey: {0}, Name: {1}".format( str(key), str(val)))
+                self.__commandList.pop(key)
+                break #break loop executing - found command to remove
+
+            notFoundCounter += 1
+            if notFoundCounter == (dictLen):
+                print("Command to remove not found.")
+                break  # break loop - command not found
+
+    #Read/Write time out
+    @property
+    def readWriteTimeOut(self):
+        return self.__rwTimeOut
+
+    @readWriteTimeOut.getter
+    def readWriteTimeOut(self):
+        print("Read/Write time out: " + str(self.__rwTimeOut))
+
+    @readWriteTimeOut.setter
+    def readWriteTimeOut(self, timeout):
+        try:
+            if (timeout < 0):
+                raise ValueError
+            else:
+                self.__rwTimeOut = timeout
+        except ValueError:
+            print("Invalid time out value. \nValue less than 0!")
