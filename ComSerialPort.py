ced4cc4e821994301d7cd8b645df3c796cf0dec6
Deuter <wojciechbernat95@gmail.com>
2020-07-26 12:14:43 +0200

    Added static methode

--- ComSerialPort.py
+++ ComSerialPort.py
@@ -1,12 +1,13 @@
 import serial
 
 
-#Communication with serial port class
+# Communication with serial port class
 class ComSerialPort:
     errorFlag = 0
-    def __init__(self,serialPortInstance, writeBuffer, readBuffer):
+
+    def __init__(self, serialPortInstance, writeBuffer, readBuffer):
         try:
-            if (type(serialPortInstance) != serial):
+            if (type(serialPortInstance) != serial.Serial):
                 raise TypeError
             else:
                 self.__serialPortInstance = serialPortInstance
@@ -15,20 +16,34 @@ class ComSerialPort:
             print("Incorrect type of serial port instances.")
 
         try:
-            if((type(writeBuffer) || type(readBuffer)) != list):
-                raise TypeError
-            else:
+            if (ComSerialPort.isList(writeBuffer) and ComSerialPort.isList(readBuffer)):
                 self.__writeBuffer = writeBuffer
                 self.__readBuffer = readBuffer
                 print("Correct type of read/write buffers.")
+            else:
+                raise TypeError
         except:
             print("Incorrect type of read/write buffers. Buffers must be list.")
-
-
-
 
     def readToBuffer(self):
         self.__readBuffer.append(self.__serialPortInstance.read_until('\n', 32))
 
     def writeFromBuffer(self):
-        self.__serialPortInstance.write
+        self.__serialPortInstance.write(ComSerialPort.lasListElement(self.__writeBuffer))
+
+    @staticmethod
+    def isList(array):
+        if type(array) == list:
+            return 1
+        else:
+            return -1
+
+    @staticmethod
+    def lasListElement(array):
+        try:
+            if (ComSerialPort.isList(array)):
+                return array[-1]
+            else:
+                raise TypeError
+        except:
+            print("Incorrect type. It is not a list.")
